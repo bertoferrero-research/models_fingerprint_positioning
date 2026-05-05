2446fe2fc6e681e3c0ad6dc7ebc345dbb958f91e
Berto Ferrero <berto@prestaquality.com>
2026-05-05 19:44:47 +0200

    update

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bertoferrero-positioning-fingerprint',
-    version='0.4.4',
+    version='0.4.5',
     packages=find_packages(),
     install_requires=[
         'tensorflow>=2.10,<2.16',
